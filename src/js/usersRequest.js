3057f85550a3b85491acaa4b5627260f31943967
“Volodymyr <vtabashniuk@outlook.com>
2023-11-06 17:03:12 +0200

    add try catch to userRequest

--- src/js/usersRequest.js
+++ src/js/usersRequest.js
@@ -2,11 +2,15 @@ import { getUsers } from './API/getUsers';
 import { dataInsertion } from './dataInsertion';
 
 usersRequest.onsubmit = async e => {
-  e.preventDefault();
-  document.getElementById('data').innerHTML = '';
-  const startDate = usersRequest.startDate.value;
-  const endDate = usersRequest.endDate.value;
-  const response = await getUsers(startDate, endDate);
-  const result = response.data;
-  dataInsertion(result);
+  try {
+    e.preventDefault();
+    document.getElementById('data').innerHTML = '';
+    const startDate = usersRequest.startDate.value;
+    const endDate = usersRequest.endDate.value;
+    const response = await getUsers(startDate, endDate);
+    const result = response.data;
+    dataInsertion(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
